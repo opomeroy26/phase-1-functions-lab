dd46587789665027c7468066f5b08157fe805c77
Olivia Pomeroy <opomeroy26@gmail.com>
2022-02-06 08:31:52 -0800

    commit tree 40a82930357803521932adac12ff494c8ea88dcf

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-// Code your solution in this file!
+// // Code your solution in this file!
+
+// function distanceFromHqInBlocks(pickup) {
+//     return Math.abs(42 - pickup)
+// }
+
+// function distanceFromHqInFeet (pickup) {
+//     return distanceFromHqInBlocks(pickup) *264
+// }
+
+// function distanceTravelledInFeet(start, end) {
+//     return Math.abs(start - end) * 264
+// }
+
+// function calculatesFarePrice(start, end) {
+//     const distance = distanceTravelledInFeet(start, end)
+//     if (distance <= 400){
+//         return 0
+//     } else if (distance > 400 && distance <= 2000) {
+//         return (distance - 400) * 0.02
+//     } else if (distance > 2000 && distance <2500){
+//         return 25
+//     } else {
+//         return 'cannot travel that far'
+//     }
+// }
+
+function distanceFromHqInBlocks(pickup) {
+    return Math.abs(pickup - 42);
+}
+
+function distanceFromHqInFeet(pickup) {
+    return (distanceFromHqInBlocks(pickup)*264);
+}
+
+function distanceTravelledInFeet(start, end) {
+    return Math.abs((start-end)*264);
+}
+
+function calculatesFarePrice(start, end) {
+   const distance = distanceTravelledInFeet(start, end);
+   if (distance < 400) {
+       return 0;
+   } else if (distance > 400 && distance <= 2000) {
+       return (distance - 400)*.02;
+   } else if (distance > 2000 && distance < 2500){
+       return 25;
+   } else {
+       return "cannot travel that far"
+   };
+}
